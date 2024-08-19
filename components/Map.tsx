0442ea62ed8c4105b553be1db854876e7dab7950
anilyavas <anil.yavas99@gmail.com>
2024-08-19 14:37:37 +0300

    custom map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,4 @@
-import Mapbox, { MapView } from '@rnmapbox/maps';
+import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_SECRET_KEY || null);
 
@@ -6,7 +6,14 @@ export default function Map() {
   return (
     <MapView
       style={{ flex: 1 }}
-      styleURL='mapbox://styles/mapbox/dark-v11'
-    ></MapView>
+      styleURL='mapbox://styles/mapbox/navigation-day-v1'
+    >
+      <LocationPuck
+        pulsing={{ isEnabled: true }}
+        puckBearingEnabled
+        puckBearing='heading'
+      />
+      <Camera followZoomLevel={5} followUserLocation />
+    </MapView>
   );
 }
